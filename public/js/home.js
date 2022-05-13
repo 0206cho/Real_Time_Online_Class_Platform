a8aa7743207ba31d91e6a66f4addff8da2f694cb
KimMinSol <0206cho@gmail.com>
2022-05-14 00:29:37 +0900

    0514_서버접속

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,5 @@
 // 페이지 들어오자마자
+var server_id = -1;
 $(function start() {
     let token = localStorage.getItem('access_token')
     console.log(token);
@@ -16,23 +17,27 @@ $(function start() {
             var c_modal = "<div class='row'>  ";
 
             for (var i = 0; i <= data.list.length; i++) { //시작시간 설정 안되어있는 경우 null -> 예약없음
-                if(data.list[i].calendar_start == null){
+                if (data.list[i].calendar_start == null) {
                     data.list[i].calendar_start = "예약없음"
                 }
+                server_id = data.list[i].srv_id;
+                console.log("serid : ", server_id)
 
                 c_modal = c_modal +
                     '<div class="col-lg-3 col-md-6 col-sm-12" style="margin-bottom: 1rem;">'
                     + '<div class="card" width="100%" height="225">'
                     + '<div class="card-body">'
-                    + '<h5 class="card-title" id="srv_name" style="font-weight: bold;">'+data.list[i].srv_name+'</h5>'
+                    + '<h5 class="card-title" id="' + data.list[i].srv_id + '" style="font-weight: bold;">' + data.list[i].srv_name + '</h5>'
                     + '다음 미팅<div>'
                     + '<svg class="bi me-1 mr-2" width="1em" height="1em">'
                     + '<use xlink:href="#calendar-event" />'
                     + '</svg>'
-                    + '<label id="calendar_start">'+data.list[i].calendar_start+'</label></div>'
-                    + '<a href="../views/notice.html" class="btn btn-enter">접속하기</a>'
+                    + '<label id="calendar_start">' + data.list[i].calendar_start + '</label></div>'
+                    + '<button type ="button" id="server-enter" class="btn btn-enter" onclick="func('+ server_id +')">접속하기</button>'
                     + '</div></div></div>'
                 $("#serverlist").html(c_modal)
+
+
             }
             c_modal += "</div>"
 
@@ -44,38 +49,41 @@ $(function start() {
             console.log(error)
         },
     });
-
-
-
 });
+
+// 접속하기 버튼
+function func(server_id) {
+    let token = localStorage.getItem('access_token')
+    console.log(token);
+
+    $.ajax({
+        headers: {
+            "authorization": 'bearer ' + token,
+        },
+        type: "GET",
+        url: "https://49.50.174.207:5000/server?srv_id=" + server_id,//  url
+        dataType: "json",
+        success: (data) => {
+            console.log(data)
+            let result = data.result;            
+
+            if (result == "1") {
+                console.log("sffs", server_id)
+                localStorage.setItem("srv_id", server_id) // 서버스토리지에 서버아이디 저장
+                location.href = './date.html';
+            } 
+        }
+    })
+}
 
 // 로그아웃
 $(function () {
     $('#logout').click(() => {
-        // $.removeCookie('mytoken', {path: '/'});
-        // alert('로그아웃!')
-        // window.location.href = "/"
-
         $.ajax({
-            url: 'https://49.50.174.207:5000/logout',
-            type: 'post',
-            data: {
-            },
-            success: function (res) {
-                let result = res.result;
-                let msg = res.msg;
-
-                if (result == 1) {
-                    alert(msg)
-                } else {
-                    alert(msg)
-                }
-            },
-            error: function (error) {
-                console.log(error)
-            },
-            complete: function (data) {
-
+            success: function (result) {
+                alert("로그아웃")
+                localStorage.clear();
+                location.href = "./join.html";
             }
         })
     });
@@ -83,7 +91,6 @@ $(function () {
 
 // 서버추가 버튼
 $(function () {
-    // alert("ssdsd")
     $('#sever_add').click(() => {
         alert('dd')
         console.log("ss")
@@ -98,12 +105,6 @@ $(function () {
             success: function (res) {
                 let result = res.result;
                 let msg = res.msg;
-
-                // if (result == 1) {
-                //     alert(msg)
-                // } else {
-                //     alert(msg)
-                // }
 
                 alert(msg)
             },
