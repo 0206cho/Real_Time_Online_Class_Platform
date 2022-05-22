22925d48828b2fbdb78b8cd3bdf5d873dfdd5e95
KimMinSol <0206cho@gmail.com>
2022-05-22 14:53:14 +0900

    0522_영상설정

--- public/js/systems.js
+++ public/js/systems.js
@@ -199,6 +199,77 @@ $("#modal_newpwconfirm").focusout(function () {
 	}
 });
 
+
+// 영상설정
+const myFace = document.getElementById("myFace");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
+let myStream;
+let cameraOff = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+async function getMedia(deviceId) {
+    const initialConstrains = {
+      audio: false,
+      video: { facingMode: "user" },
+    };
+    const cameraConstraints = {
+      audio: false,
+      video: { deviceId: { exact: deviceId } },
+    };
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstrains
+          );
+          myFace.srcObject = myStream;
+          if (!deviceId) {
+            await getCameras();
+          }
+        } catch (e) {
+          console.log(e);
+        }
+    }
+    getMedia();
+    function handleCameraClick() {
+      myStream
+        .getVideoTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
+        if (cameraOff) {
+            cameraBtn.innerText = "카메라 끄기";
+            cameraOff = false;
+          } else {
+            cameraBtn.innerText = "카메라 켜기";
+            cameraOff = true;
+          }
+        }
+        
+        async function handleCameraChange() {
+          await getMedia(camerasSelect.value);
+        }
+        
+        // muteBtn.addEventListener("click", handleMuteClick);
+        cameraBtn.addEventListener("click", handleCameraClick);
+        camerasSelect.addEventListener("input", handleCameraChange);
+
 //영상테스트
 var myVideoStream = document.getElementById('myVideo')     // make it a global variable
 function getVideo() {
